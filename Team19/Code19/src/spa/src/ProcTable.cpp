af8d510bac6dd32e7c6281d4421d21cac8e43a95
CheyanneSim <CheyanneSim@users.noreply.github.com>
2021-01-27 16:09:48 +0800

    Add ProcTable

--- Team19/Code19/src/spa/src/ProcTable.cpp
+++ Team19/Code19/src/spa/src/ProcTable.cpp
@@ -1,5 +1,67 @@
-//
-// Created by Cheyanne Sim on 27/1/21.
-//
+#pragma once
 
+#include <iostream>
 #include "ProcTable.h"
+
+ProcTable::ProcTable() = default;
+
+int ProcTable::getProcID(std::string procName) {
+    if (procNameIDMap.find(procName) == procNameIDMap.end()) {
+        return -1;
+    } else {
+        return procNameIDMap.find(procName)->second;
+    }
+}
+
+std::string ProcTable::getProcName(int procID) {
+    try {
+        return procNames.at(procID);
+    } catch (std::out_of_range const& e) {
+        std::cout << "Index out of range: No procedure with ID " << procID << " in procNameIDMap." << std::endl;
+        throw;
+    }
+}
+
+std::pair<int, int> ProcTable::getProcRange(int procID) {
+    if (procStmtMap.find(procID) == procStmtMap.end()) {
+        std::cout << "Index out of range: No procedure with ID " << procID << " in procStmtMap." << std::endl;
+        throw std::out_of_range("ProcID not found in procStmtMap.");
+    } else {
+        return procStmtMap.find(procID)->second;
+    }
+}
+
+std::vector<std::string> const &ProcTable::getAllProcNames() const {
+    return procNames;
+}
+
+std::vector<std::string> ProcTable::convertProcIDsToNames(std::vector<int> procIDs) {
+    std::vector<std::string> result(getSize());
+    for (int id : procIDs) {
+        result.push_back(getProcName(id));
+    }
+    return result;
+}
+
+int ProcTable::getSize() {
+    return procNames.size();
+}
+
+int ProcTable::storeProcName(std::string procName) {
+    if (hasProc(procName)) {
+        return getProcID(procName);
+    } else {
+        int id = getSize();
+        procNameIDMap.insert({procName, id});
+        procNames.push_back(procName);
+        return id;
+    }
+}
+
+bool ProcTable::storeProcStmt(int procID, int startStmt, int endStmt) {
+    return procStmtMap.insert({procID, std::make_pair(startStmt, endStmt)}).second;
+}
+
+bool ProcTable::hasProc(std::string procName) {
+    return procNameIDMap.find(procName) != procNameIDMap.end();
+}
--- Team19/Code19/src/spa/src/ProcTable.h
+++ Team19/Code19/src/spa/src/ProcTable.h
@@ -1,14 +1,52 @@
-//
-// Created by Cheyanne Sim on 27/1/21.
-//
+#pragma once
 
-#ifndef AUTOTESTER_PROCTABLE_H
-#define AUTOTESTER_PROCTABLE_H
-
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 class ProcTable {
+public:
+
+    // Constructor for ProcTable
+    ProcTable();
+
+    // Returns the procID corresponding to the procName if it exists.
+    // otherwise, return -1.
+    int getProcID(std::string procName);
+
+    // Returns the procName corresponding to the procID if it exists. Throws out of index exception otherwise.
+    std::string getProcName(int procID);
+
+    // Returns the procedure's startStmt and endStmt if it exists. Throws out of index exception otherwise.
+    std::pair<int, int> getProcRange(int procID);
+
+    // returns a const reference to the vector of all procedure names stored.
+    std::vector<std::string> const &getAllProcNames() const;
+
+    // returns a vector of procedure names corresponding to the vector of procedure IDs given.
+    std::vector<std::string> convertProcIDsToNames(std::vector<int> procIDs);
+
+    // Returns the number of procedures in the table.
+    int getSize();
+
+    // stores the procedure into the procNameIDMap and procNames. Returns the ID of the procedure.
+    int storeProcName(std::string procName);
+
+    // stores the procedure that spans from startStmt to endStmt into the ProcStmtMap. Returns true if insertion took place.
+    bool storeProcStmt(int procID, int startStmt, int endStmt);
+
+    // Returns true if the procedure has already been added before; otherwise, returns false.
+    bool hasProc(std::string procName);
+
+private:
+    // A map that stores procedure name as key and ID as value.
+    std::unordered_map<std::string, int> procNameIDMap;
+
+    // A vector that stores all the procedure names such that the indexes are the ID of the procedure.
+    std::vector<std::string> procNames;
+
+    // A map that stores procedure id as key, and <startStmt, endStmt> as value.
+    std::unordered_map<int, std::pair<int, int> > procStmtMap;
 
 };
 
-
-#endif //AUTOTESTER_PROCTABLE_H
