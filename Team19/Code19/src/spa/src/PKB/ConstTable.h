d45bdbbfdcd74499b39b3196891a1b3fe18a6c48
ambervoong <voongyuxuan@gmail.com>
2021-01-28 10:41:59 +0800

    Reorganise PKB files
    Changes CMakeList to glob for .cpp and .h files in /src recursively.
    Move PKB into its own folder. Adds ConstTable class.

---
+++ Team19/Code19/src/spa/src/PKB/ConstTable.h
@@ -0,0 +1,10 @@
+#ifndef INC_20S2_CP_SPA_TEAM_19_CONSTTABLE_H
+#define INC_20S2_CP_SPA_TEAM_19_CONSTTABLE_H
+
+
+class ConstTable {
+
+};
+
+
+#endif //INC_20S2_CP_SPA_TEAM_19_CONSTTABLE_H
--- Team19/Code19/src/spa/src/PKB/PKB.cpp
+++ Team19/Code19/src/spa/src/PKB/PKB.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 #include "PKB.h"
-#include "TNode.h"
+#include "../TNode.h"
 
 int PKB::setProcToAST(PROC p, TNode* r) {
 	return 0;
