1e656ac49a6d13bd495ae1e1734cc2a92067ca76
zixinn <ng.zixin99@gmail.com>
2021-02-05 23:15:34 +0800

    Add FollowsEvaluator

---
+++ Team19/Code19/src/spa/src/PQL/FollowsEvaluator.cpp
@@ -0,0 +1,96 @@
+#include "FollowsEvaluator.h"
+
+using namespace std;
+
+FollowsEvaluator::FollowsEvaluator() {
+
+}
+
+bool FollowsEvaluator::evaluate(unordered_map<string, string> declarations, Clause clause, unordered_map<string, vector<int>>& tempResults) {
+    string firstArg = clause.getArgs().at(0);
+    string secondArg = clause.getArgs().at(1);
+    string firstType = getArgType(firstArg, declarations);
+    string secondType = getArgType(secondArg, declarations);
+
+    if (firstType == UNDERSCORE_ && secondType == UNDERSCORE_) { // _, _
+//        return PKB::follows->getFollowsSize() > 0;
+        return true; // to remove
+    }
+
+    if (firstType == INTEGER_ && secondType == INTEGER_) { // known, known
+//        return PKB::follows->isFollows(stoi(firstArg), stoi(secondArg));
+        return true; // to remove
+
+    } else if (firstType == INTEGER_ && secondType != INTEGER_) { // known, s or known, _
+//        int follower = PKB::follows->getFollower(stoi(firstArg));
+        int follower; // to remove
+        if (follower == -1) {
+            return false;
+        }
+        if (secondType != UNDERSCORE_) {
+            vector<int> res;
+            bool nonEmpty = intersectSingleSynonym(vector<int>{ follower }, selectAll(secondType), res);
+            if (nonEmpty) {
+                tempResults[secondArg] = res;
+            }
+            return nonEmpty;
+        }
+        return true;
+
+    } else if (firstType != INTEGER_ && secondType == INTEGER_) { // s, known or _, known
+//        int followee = PKB::follows->getFollowee(stoi(secondArg));
+        int followee; // to remove
+        if (followee == -1) {
+            return false;
+        }
+        if (firstType != UNDERSCORE_) {
+            vector<int> res;
+            bool nonEmpty = intersectSingleSynonym(vector<int>{ followee }, selectAll(firstType), res);
+            if (nonEmpty) {
+                tempResults[firstArg] = res;
+            }
+            return nonEmpty;
+        }
+        return true;
+
+    } else { // s1, s2 or s, _ or _, s
+        if (firstArg == secondArg) {
+            return false;
+        }
+//        pair<vector<int>, vector<int>> allFollows = PKB::follows->getAllFollows();
+        pair<vector<int>, vector<int>> allFollows; // to remove
+        if (allFollows.first.empty()) {
+            return false;
+        }
+        if (firstType != UNDERSCORE_ && secondType != UNDERSCORE_) { // s1, s2
+            pair<vector<int>, vector<int>> allCorrectType = make_pair(selectAll(firstType), selectAll(secondType));
+            pair<vector<int>, vector<int>> res;
+            bool nonEmpty = intersectDoubleSynonym(allFollows, allCorrectType, res);
+            if (nonEmpty) {
+                tempResults[firstArg] = res.first;
+                tempResults[secondArg] = res.second;
+            }
+            return nonEmpty;
+        } else if (firstType != UNDERSCORE_) { // s, _
+            vector<int> allCorrectType = selectAll(firstType);
+            vector<int> res;
+            bool nonEmpty = intersectSingleSynonym(allFollows.first, allCorrectType, res);
+            if (nonEmpty) {
+                tempResults[firstArg] = res;
+            }
+            return nonEmpty;
+        } else { // _, s
+            vector<int> allCorrectType = selectAll(secondType);
+            vector<int> res;
+            bool nonEmpty = intersectSingleSynonym(allFollows.second, allCorrectType, res);
+            if (nonEmpty) {
+                tempResults[secondArg] = res;
+            }
+            return nonEmpty;
+        }
+    }
+}
+
+FollowsEvaluator::~FollowsEvaluator() {
+
+}
