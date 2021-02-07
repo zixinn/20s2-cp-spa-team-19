ffea4f215661a0db0001ca5c20d42657f51663ef
Ang Lu Ling <ang.lu.shing99@gmail.com>
2021-02-07 19:03:17 +0800

    edit parseProg and test

--- Team19/Code19/src/unit_testing/src/SP/TestParseProc.cpp
+++ Team19/Code19/src/unit_testing/src/SP/TestParseProc.cpp
@@ -39,117 +39,83 @@ TEST_CASE("Parse Proc Test") {
     REQUIRE(stmt_lst->toString() == expectedString);
 }
 
-// keywords are allowed, lexer will not detect as name, but are still legal
-TEST_CASE("Parse Proc - Keyword Test") {
+TEST_CASE("Parse Prog - Proc Test") {
     std::vector<sp::Token*> stubTokens{
         new sp::Token(sp::Token::TokenType::PROC, "procedure"),
-        new sp::Token(sp::Token::TokenType::PROC, "procedure"),
+        new sp::Token(sp::Token::TokenType::NAME, "star"),
         new sp::Token(sp::Token::TokenType::LBRACE, "{"),
+        new sp::Token(sp::Token::TokenType::NAME, "x"),
+        new sp::Token(sp::Token::TokenType::ASSIGN, "="),
+        new sp::Token(sp::Token::TokenType::CONST, "1"),
+        new sp::Token(sp::Token::TokenType::SEMICOLON, ";"),
         new sp::Token(sp::Token::TokenType::RBRACE, "}"),
-        new sp::Token(sp::Token::TokenType::EOFF, "EOF"),
         new sp::Token(sp::Token::TokenType::PROC, "procedure"),
-        new sp::Token(sp::Token::TokenType::READ, "read"),
+        new sp::Token(sp::Token::TokenType::NAME, "moon"),
         new sp::Token(sp::Token::TokenType::LBRACE, "{"),
-        new sp::Token(sp::Token::TokenType::RBRACE, "}"),
-        new sp::Token(sp::Token::TokenType::EOFF, "EOF"),
-        new sp::Token(sp::Token::TokenType::PROC, "procedure"),
-        new sp::Token(sp::Token::TokenType::PRINT, "print"),
-        new sp::Token(sp::Token::TokenType::LBRACE, "{"),
-        new sp::Token(sp::Token::TokenType::RBRACE, "}"),
-        new sp::Token(sp::Token::TokenType::EOFF, "EOF"),
-        new sp::Token(sp::Token::TokenType::PROC, "procedure"),
-        new sp::Token(sp::Token::TokenType::CALL, "call"),
-        new sp::Token(sp::Token::TokenType::LBRACE, "{"),
-        new sp::Token(sp::Token::TokenType::RBRACE, "}"),
-        new sp::Token(sp::Token::TokenType::EOFF, "EOF"),
-        new sp::Token(sp::Token::TokenType::PROC, "procedure"),
-        new sp::Token(sp::Token::TokenType::WHILE, "while"),
-        new sp::Token(sp::Token::TokenType::LBRACE, "{"),
-        new sp::Token(sp::Token::TokenType::RBRACE, "}"),
-        new sp::Token(sp::Token::TokenType::EOFF, "EOF"),
-        new sp::Token(sp::Token::TokenType::PROC, "procedure"),
-        new sp::Token(sp::Token::TokenType::IF, "if"),
-        new sp::Token(sp::Token::TokenType::LBRACE, "{"),
-        new sp::Token(sp::Token::TokenType::RBRACE, "}"),
-        new sp::Token(sp::Token::TokenType::EOFF, "EOF"),
-        new sp::Token(sp::Token::TokenType::PROC, "procedure"),
-        new sp::Token(sp::Token::TokenType::THEN, "then"),
-        new sp::Token(sp::Token::TokenType::LBRACE, "{"),
-        new sp::Token(sp::Token::TokenType::RBRACE, "}"),
-        new sp::Token(sp::Token::TokenType::EOFF, "EOF"),
-        new sp::Token(sp::Token::TokenType::PROC, "procedure"),
-        new sp::Token(sp::Token::TokenType::ELSE, "else"),
-        new sp::Token(sp::Token::TokenType::LBRACE, "{"),
+        new sp::Token(sp::Token::TokenType::NAME, "x"),
+        new sp::Token(sp::Token::TokenType::ASSIGN, "="),
+        new sp::Token(sp::Token::TokenType::CONST, "1"),
+        new sp::Token(sp::Token::TokenType::SEMICOLON, ";"),
         new sp::Token(sp::Token::TokenType::RBRACE, "}"),
         new sp::Token(sp::Token::TokenType::EOFF, "EOF"),
     };
 
     auto l = new LexerStub(stubTokens);     //new keyword gets me a ptr to LexerStub
     Parser p = Parser(l);
-
-    std::vector<std::string> identifiers{ "procedure", "read", "print", "call", "while",
-        "if", "then", "else" };
+    ast::Program* prog = p.parseProgram();
+    std::vector<ast::Proc*> procs = prog->getProcedures();
+    std::vector<std::string> identifiers{ "star", "moon" };
 
     for (int i = 0; i < identifiers.size(); i++) {
-        ast::Proc* proc = p.parseProc();
+        ast::Proc* proc = procs[i];
         REQUIRE(proc->compareToken(new sp::Token(sp::Token::TokenType::PROC, "procedure")));
         REQUIRE(proc->getName()->getVal() == identifiers[i]);
 
         ast::StmtLst* stmt_lst = proc->getStmtLst();
-        REQUIRE((*stmt_lst).getStatements().size() == 0);
+        REQUIRE((*stmt_lst).getStatements().size() == 1);
 
-        std::string expectedString = "{\n}\n";
+        std::string expectedString = "{\n    x = 1;\n}\n";
         REQUIRE(stmt_lst->toString() == expectedString);
-        
-        //skip EOF
-        p.nextToken();
+
     }
-    
+
 }
 
-/*
+
 TEST_CASE("Parse Prog - Proc - Keyword Test") {
     std::vector<sp::Token*> stubTokens{
         new sp::Token(sp::Token::TokenType::PROC, "procedure"),
         new sp::Token(sp::Token::TokenType::PROC, "procedure"),
         new sp::Token(sp::Token::TokenType::LBRACE, "{"),
         new sp::Token(sp::Token::TokenType::RBRACE, "}"),
-        new sp::Token(sp::Token::TokenType::EOFF, "EOF"),
         new sp::Token(sp::Token::TokenType::PROC, "procedure"),
         new sp::Token(sp::Token::TokenType::READ, "read"),
         new sp::Token(sp::Token::TokenType::LBRACE, "{"),
         new sp::Token(sp::Token::TokenType::RBRACE, "}"),
-        new sp::Token(sp::Token::TokenType::EOFF, "EOF"),
         new sp::Token(sp::Token::TokenType::PROC, "procedure"),
         new sp::Token(sp::Token::TokenType::PRINT, "print"),
         new sp::Token(sp::Token::TokenType::LBRACE, "{"),
         new sp::Token(sp::Token::TokenType::RBRACE, "}"),
-        new sp::Token(sp::Token::TokenType::EOFF, "EOF"),
         new sp::Token(sp::Token::TokenType::PROC, "procedure"),
         new sp::Token(sp::Token::TokenType::CALL, "call"),
         new sp::Token(sp::Token::TokenType::LBRACE, "{"),
         new sp::Token(sp::Token::TokenType::RBRACE, "}"),
-        new sp::Token(sp::Token::TokenType::EOFF, "EOF"),
         new sp::Token(sp::Token::TokenType::PROC, "procedure"),
         new sp::Token(sp::Token::TokenType::WHILE, "while"),
         new sp::Token(sp::Token::TokenType::LBRACE, "{"),
         new sp::Token(sp::Token::TokenType::RBRACE, "}"),
-        new sp::Token(sp::Token::TokenType::EOFF, "EOF"),
         new sp::Token(sp::Token::TokenType::PROC, "procedure"),
         new sp::Token(sp::Token::TokenType::IF, "if"),
         new sp::Token(sp::Token::TokenType::LBRACE, "{"),
         new sp::Token(sp::Token::TokenType::RBRACE, "}"),
-        new sp::Token(sp::Token::TokenType::EOFF, "EOF"),
         new sp::Token(sp::Token::TokenType::PROC, "procedure"),
         new sp::Token(sp::Token::TokenType::THEN, "then"),
         new sp::Token(sp::Token::TokenType::LBRACE, "{"),
         new sp::Token(sp::Token::TokenType::RBRACE, "}"),
-        new sp::Token(sp::Token::TokenType::EOFF, "EOF"),
         new sp::Token(sp::Token::TokenType::PROC, "procedure"),
         new sp::Token(sp::Token::TokenType::ELSE, "else"),
         new sp::Token(sp::Token::TokenType::LBRACE, "{"),
         new sp::Token(sp::Token::TokenType::RBRACE, "}"),
-        new sp::Token(sp::Token::TokenType::EOFF, "EOF"),
         new sp::Token(sp::Token::TokenType::EOFF, "EOF"),
     };
 
@@ -174,4 +140,4 @@ TEST_CASE("Parse Prog - Proc - Keyword Test") {
     }
 
 }
-*/
+
