215c34da54aa92ed9b695aeb545720e3e340b542
uwu <e0031374@gmail.com>
2021-02-09 13:59:23 +0800

    Add ParseIf
    Failing some conditional cases, will need to ammend at later date

--- Team19/Code19/src/spa/src/SP/Parser.cpp
+++ Team19/Code19/src/spa/src/SP/Parser.cpp
@@ -104,6 +104,8 @@ ast::Stmt* Parser::parseStmt() {
 		return this->parseReadStmt();
 	} else if (this->currTokenIs(sp::Token::TokenType::WHILE)) {
 		return this->parseWhileStmt();
+	} else if (this->currTokenIs(sp::Token::TokenType::IF)) {
+		return this->parseIfStmt();
 	}
 	throw this->genError("ParseStmt received unexpected token: " + this->currLiteral());
 }
@@ -469,6 +471,54 @@ ast::WhileStmt* Parser::parseWhileStmt() {
 	}
 
 	return new ast::WhileStmt(this->getPlusPC(), tok, cond_expr, stmt_lst);
+}
+
+ast::IfStmt* Parser::parseIfStmt() {
+	//throw "NOT READY";
+	auto tok = this->currToken;
+	if (!this->currTokenIs(sp::Token::TokenType::IF)) {
+		throw this->genError("ParseIf expected IF, encountered: " + this->currLiteral());
+	}
+	if (!this->expectPeek(sp::Token::TokenType::LPAREN)) {
+		throw this->genError("ParseIf expected LPAREN (, encountered: " + this->peekToken->getLiteral());
+	}
+	auto cond_expr = this->parseCondExpr(ParserUtils::CondExprPrecedence::LOWEST);
+
+	if (!this->currTokenIs(sp::Token::TokenType::RPAREN)) {
+		throw this->genError("ParseIf expected RPAREN ), encountered: " + this->currLiteral());
+	}
+
+	if (!this->expectPeek(sp::Token::TokenType::THEN)) {
+		throw this->genError("ParseIf expected THEN, encountered: " + this->peekToken->getLiteral());
+	}
+
+	if (!this->expectPeek(sp::Token::TokenType::LBRACE)) {
+		throw this->genError("ParseIf expected LBRACE {, encountered: " + this->peekToken->getLiteral());
+	}
+	this->nextToken();
+
+	auto csq_lst = this->parseStmtLst();
+
+	if (!this->currTokenIs(sp::Token::TokenType::RBRACE)) {
+		throw this->genError("ParseIf expected RBRACE }, encountered: " + this->currLiteral());
+	}
+
+	if (!this->expectPeek(sp::Token::TokenType::ELSE)) {
+		throw this->genError("ParseIf expected ELSE, encountered: " + this->peekToken->getLiteral());
+	}
+
+	if (!this->expectPeek(sp::Token::TokenType::LBRACE)) {
+		throw this->genError("ParseWhile expected LBRACE {, encountered: " + this->peekToken->getLiteral());
+	}
+	this->nextToken();
+
+	auto alt_lst = this->parseStmtLst();
+
+	if (!this->currTokenIs(sp::Token::TokenType::RBRACE)) {
+		throw this->genError("ParseWhile expected RBRACE }, encountered: " + this->currLiteral());
+	}
+
+	return new ast::IfStmt(this->getPlusPC(), tok, cond_expr, csq_lst, alt_lst);
 }
 
 //std::string Parser::genError(std::string str) {
---
+++ Team19/Code19/src/unit_testing/src/SP/TestParseIf.cpp
@@ -0,0 +1,100 @@
+//#include "Parser.h"
+#include <string>
+#include <vector>
+#include <iostream>
+#include "AST/Index.h"
+#include "SP/Parser.h"
+#include "SP/ParserUtils.h"
+
+#include "catch.hpp"
+using namespace std;
+
+// test for some bracketings assign expr
+TEST_CASE("ParseLexer If - Test") {
+
+    //std::string input = "if = v + x * y + z * t;";    // if not implemented yet
+    std::vector<std::pair<std::string, std::string>> tests{
+        {
+            "if (flag == 1) then {} else {}",
+            "if (flag == 1) then {\n}\nelse {\n}\n",
+        },
+        {
+            "if ((flag) == 1) then {} else {}",
+            "if (flag == 1) then {\n}\nelse {\n}\n",
+        },
+        {
+            "if ((flag == (1 * 5))|| (3 > 2)) then { x = (2 + 1); } else { print call; }\n",
+            "if ((flag == (1 * 5)) || (3 > 2)) then {\n    x = (2 + 1);\n}\nelse {\n    print call;\n}\n",
+        },
+        {
+            "if (flag == 1) then {while ((a + b) > c) { bb = dd;}} else {}",
+            "if (flag == 1) then {\n    while ((a + b) > c) {\n    bb = dd;\n}\n\n}\nelse {\n}\n",
+        },
+    };
+
+    for (int i = 0; i < tests.size(); ++i) {
+        std::string input = std::get<0>(tests[i]);
+        std::string expected = std::get<1>(tests[i]);
+
+        /** begin ritual to Summon Parser **/
+        std::vector<sp::Token> actual_tok;
+        std::vector<sp::Token*> tok_ptrs;
+        ParserUtils::StringToTokenPtrs(input, actual_tok, tok_ptrs);
+        auto l = new LexerStub(tok_ptrs);
+        auto p = Parser(l);
+        /** Parser now ready for use      **/
+
+        try {
+            ast::Stmt* is = p.parseStmt();
+            REQUIRE(is->toString() == expected);
+        }
+        catch (sp::ParserException &ex) {
+            INFO(ex.what());
+            INFO("TestCase: " + input + ", Exception Thrown");
+            REQUIRE(false);
+        }
+    }
+}
+
+TEST_CASE("ParseLexer If - Exceptions, Test") {
+    //std::string input = "if = v + x * y + z * t;";    // if not implemented yet
+    std::vector<std::pair<std::string, std::string>> tests{
+        //{   // failing this, ill fix this after iter1 promise
+        //    "if (a + 5) then { } else {}",
+        //    "if (a + 5) then { } else {}",
+        //},
+        { // relying on others like IF/WHILE to catch this
+            "if (a == b) && (c < 5)) then {} else {}",
+            "if (a == b) && (c < 5)) then {} else {}",
+        },
+        {
+            "if (a == b) && c < 5) then {} else {}",
+            "if (a == b) && c < 5) then {} else {}",
+        },
+    };
+
+    for (int i = 0; i < tests.size(); ++i) {
+        std::string input = std::get<0>(tests[i]);
+        std::string expected = std::get<1>(tests[i]);
+
+        /** begin ritual to Summon Parser **/
+        std::vector<sp::Token> actual_tok;
+        std::vector<sp::Token*> tok_ptrs;
+        ParserUtils::StringToTokenPtrs(input, actual_tok, tok_ptrs);
+        auto l = new LexerStub(tok_ptrs);
+        auto p = Parser(l);
+        /** Parser now ready for use      **/
+
+        try {
+            ast::Stmt* ce = p.parseStmt();
+            //REQUIRE(ce->toString() == expected);
+            INFO("Exception Expected: TestCase: " + input + ", got: " + ce->toString());
+            REQUIRE(false);
+        }
+        catch (sp::ParserException &ex) {
+            INFO(ex.what());
+            //INFO("TestCase: " + input + ", Exception Thrown");
+            REQUIRE(true);
+        }
+    }
+}
