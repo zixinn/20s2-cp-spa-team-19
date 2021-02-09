d7cdd2101ed3a0e2d5f1ad0f1b5e2d774b5961a7
uwu <e0031374@gmail.com>
2021-02-10 04:02:59 +0800

    Added CondExprUtils ParseRelExpr
    note refactor out seekL and seekR

--- Team19/Code19/src/spa/src/SP/CondExprUtils.h
+++ Team19/Code19/src/spa/src/SP/CondExprUtils.h
@@ -8,7 +8,6 @@
 // function pointers for ExprParsing
 namespace CondExprUtils {
 
-
 	std::string VectorToString(std::vector<sp::Token*>& input) {
 		std::string out = "";
 		for (int i = 0; i < input.size(); ++i) {
@@ -29,6 +28,92 @@ namespace CondExprUtils {
 		for (int i = left; i <= right; ++i) {
 			output.push_back(input[i]);
 		}
+	}
+
+	// for use in ParseRelExpr
+	void CheckExpr(std::vector<sp::Token*> input, std::string comp) {
+		// begin ritual
+        auto l = new LexerStub(input);
+        auto p = Parser(l);
+        // Parser now ready for use
+		try {
+			ast::Expr* ce = p.parseExpr(ParserUtils::ExprPrecedence::LOWEST);
+		}
+		catch (sp::ParserException& ex) {
+			throw sp::UtilsException("" + comp + " :: " + ex.what());
+		}
+	}
+
+	// expect leftmost and rightmost to be brackets
+	void ParseRelExpr(std::vector<sp::Token*>& input, std::vector<sp::Token*>& output, int index) {
+
+		//std::cout << "IN: " + std::to_string(index) << std::endl;
+		int left = index - 1;
+		int right = index + 1;
+
+		// go left until find (, l_lparen == 1 when we have an extra (
+		int l_lparen = 0;
+		while (left > 0) {
+			if (input[left]->getType() == sp::Token::TokenType::RPAREN) { l_lparen--; }
+			if (input[left]->getType() == sp::Token::TokenType::LPAREN) { l_lparen++; }
+			if (l_lparen == 1) { break; }
+			left--;
+		}
+		//std::cout << "LEFT: " + std::to_string(left) << std::endl;
+		//std::cout << "l_lparen: " + std::to_string(l_lparen) << std::endl;
+
+		// if not LPAREN, may have reached start of list
+		if (input[left]->getType() != sp::Token::TokenType::LPAREN) {
+			throw sp::UtilsException("ParseRelExpr (LPAREN) expected LPAREN instead encountered: " + input[left]->getLiteral());
+		}
+
+		// go right until find ), r_lparen == -1 when we have an extra )
+		int r_lparen = 0;
+		while (right < input.size()) {
+			if (input[right]->getType() == sp::Token::TokenType::RPAREN) { r_lparen--; }
+			if (input[right]->getType() == sp::Token::TokenType::LPAREN) { r_lparen++; }
+			if (r_lparen == -1) { break; }
+			right++;
+		}
+		std::cout << "RIGHT: " + std::to_string(right) << std::endl;
+
+		// if not RPAREN, may have reached end of list
+		if (input[right]->getType() != sp::Token::TokenType::RPAREN) {
+			throw sp::UtilsException("ParseRelExpr (RPAREN) expected RPAREN instead encountered: " + input[right]->getLiteral());
+		}
+
+		// LHS expr check, left+1 to not include (
+		std::vector<sp::Token*> tmp_vec_lhs;
+		VectorSlice(input, tmp_vec_lhs, left+1, index-1);
+		CheckExpr(tmp_vec_lhs, "ParseRelExpr (LHS)");
+
+		// RHS expr check, right-1 to not inclide )
+		std::vector<sp::Token*> tmp_vec_rhs;
+		VectorSlice(input, tmp_vec_rhs, index+1, right-1);
+		CheckExpr(tmp_vec_rhs, "ParseRelExpr (RHS)");
+
+		// copy over into return vector
+		// DO NOT consume the ( and ), relExpr is valid without them, its for the others to consume
+		for (int i = 0; i < input.size(); i++) {
+			// this copis brackets too
+			if (i <= left || right <= i) {
+				output.push_back(input[i]);
+			}
+			if (i == index) {
+				//output.push_back(new sp::Token(sp::Token::TokenType::BOOL, "CondExprUtils.h"));
+				output.push_back(new sp::Token(sp::Token::TokenType::BOOL, "BOOL"));
+			}
+		}
+
+		std::cout << "CHECKEXPR: " + std::to_string(3) << std::endl;
+	}
+
+
+	// outermost logic
+	ast::CondExprBag ParseCondExpr(std::vector<sp::Token*>& input) {
+		// find all relexpr and deal with those
+		// then call ParseCondExprInner, which dispatches between NOT and &&
+		throw "NOT READY";
 	}
 
 	void ParseNot(std::vector<sp::Token*>& input,std::vector<sp::Token*>& output, int index) {
@@ -53,7 +138,7 @@ namespace CondExprUtils {
 		}
 
 		std::vector<sp::Token*> tmp_vec;
-		// ParseRelExprAlter(input, tmp_vec);
+		// ParseCondExprInner(input, tmp_vec);	// no relexpr anymore all done in first pass
 		// if no error, then we can toss away tmp_vec
 		// copy input into output vector
 
--- Team19/Code19/src/unit_testing/src/SP/TestCondExprUtils.cpp
+++ Team19/Code19/src/unit_testing/src/SP/TestCondExprUtils.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 // test for some bracketings assign expr
 // NOTE: for some reason, Lexer::Tokenize expects a RPAREN ) or SEMICOLON ; to terminate string
-TEST_CASE("CondExprUtils2 - Test") {
+TEST_CASE("CondExprUtils - Test") {
     std::string input = "0 1 2 3 4 5 6 6 8 9";
 
 
@@ -34,5 +34,58 @@ TEST_CASE("CondExprUtils2 - Test") {
         REQUIRE(false);
     }
 
+}
+
+TEST_CASE("CondExprUtils - ParseRelExpr Test") {
+
+    std::vector<std::tuple<std::string, int, std::string>> tests{
+        {
+            "(flag >= b)", 2,   // >= at index 2
+            "( BOOL )",
+        },
+        {
+            "(flag >= b + 3 * 4)", 2,   // >= at index 2
+            "( BOOL )",
+        },
+        {
+            "(flag >= (((b) + 3) * 4))", 2,   // >= at index 2
+            "( BOOL )",
+        },
+        {
+            "((flag) + 5 * 3 >= (((b) + 3) * 4))", 8,   // >= at index 8
+            "( BOOL )",
+        },
+    };
+
+    for (int ii = 0; ii < tests.size(); ii++) {
+        std::string input = std::get<0>(tests[ii]);
+        int index = std::get<1>(tests[ii]);
+        std::string expected = std::get<2>(tests[ii]);
+
+
+        //  generate token* vector from string
+        std::vector<sp::Token> actual_tok;
+        std::vector<sp::Token*> tok_ptrs;
+        ParserUtils::StringToTokenPtrs(input, actual_tok, tok_ptrs);
+        if (tok_ptrs.back()->getType() == sp::Token::TokenType::EOFF) { tok_ptrs.pop_back(); }
+        //
+
+        std::vector<sp::Token*> out_ptrs;
+        try {
+            CondExprUtils::ParseRelExpr(tok_ptrs, out_ptrs, index);
+            auto result = CondExprUtils::VectorToString(out_ptrs);
+            REQUIRE(result == expected);
+        }
+        catch (sp::UtilsException& ex) {
+            INFO(ex.what());
+            INFO("UE: Test case: " + input);
+            REQUIRE(false);
+        }
+        catch (sp::ParserException& ex) {
+            INFO(ex.what());
+            INFO("PE: Test case: " + input);
+            REQUIRE(false);
+        }
+    }
 }
 
