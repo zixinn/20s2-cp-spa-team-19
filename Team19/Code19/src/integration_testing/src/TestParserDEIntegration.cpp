384f18e35ad05c6904e290ae2e66227eb1160b98
ambervoong <voongyuxuan@gmail.com>
2021-02-14 16:50:46 +0800

    Add triple-nested if test, expr test

--- Team19/Code19/src/integration_testing/src/TestParserDEIntegration.cpp
+++ Team19/Code19/src/integration_testing/src/TestParserDEIntegration.cpp
@@ -47,7 +47,59 @@ TEST_CASE("Simple - Parser-DE Test") {
 
     // Check Modifies
     REQUIRE(PKB::modifies->getStmtsModifies(varID) == unordered_set<ID>{ 1});
-    REQUIRE(PKB::modifies->getStmtsModifies(varID1) == unordered_set<ID>{  });
+    REQUIRE(PKB::modifies->getStmtsModifies(varID1) == unordered_set<ID>{ });
+}
+
+TEST_CASE("Complicated Expression - Parser-DE Test") {
+    /*
+    procedure star {
+      1   x = (1) + (2 * ((5 * 0 * 2 + 5 / 1) * a) / 20 * (1097 - pollution));
+     }
+     */
+    string input = "procedure star {x = (1) + (2 * ((5 * 0 * 2 + 5 / 1) * a) / 20 * (1097 - pollution));} ";
+    std::vector<sp::Token> actual_tok;
+    std::vector<sp::Token*> tok_ptrs;
+    ParserUtils::StringToTokenPtrs(input, actual_tok, tok_ptrs);
+    auto l = new LexerStub(tok_ptrs);
+    auto p = Parser(l);
+
+    REQUIRE(p.parse()); // no errors
+
+    // Check StmtLst
+    REQUIRE(PKB::stmtLstTable->hasStmtLst(1) == true);
+    REQUIRE(PKB::stmtLstTable->hasStmtLst(2) == false);
+
+    // Check varNames
+    ID varID = PKB::varTable->getVarID("x");
+    REQUIRE(varID == 0);    // varTable ID starts indexing at 0
+    REQUIRE(PKB::varTable->getVarName(varID) == "x");
+    ID varID1 = PKB::varTable->getVarID("a");
+    REQUIRE(varID1 == 1);
+    REQUIRE(PKB::varTable->getVarName(varID1) == "a");
+    ID varID2 = PKB::varTable->getVarID("pollution");
+    REQUIRE(varID2 == 2);
+    REQUIRE(PKB::varTable->getVarName(varID2) == "pollution");
+
+    // Check consts used in AssignStmt
+    REQUIRE(PKB::constTable->hasConst("0") == true);
+    REQUIRE(PKB::constTable->hasConst("1") == true);
+    REQUIRE(PKB::constTable->hasConst("2") == true);
+    REQUIRE(PKB::constTable->hasConst("5") == true);
+    REQUIRE(PKB::constTable->hasConst("20") == true);
+    REQUIRE(PKB::constTable->hasConst("1097") == true);
+    REQUIRE(PKB::constTable->hasConst("3") == false);
+
+    // Check Follows
+    REQUIRE(PKB::follows->getFollower(1) == -1); // no Followers
+
+    // Check Uses
+    REQUIRE(PKB::uses->getStmtsUses(varID1) == unordered_set<StmtNum>{ 1 });
+    REQUIRE(PKB::uses->getStmtsUses(varID2) == unordered_set<StmtNum>{ 1 });
+    REQUIRE(PKB::uses->getVarsUsedByProc(0) == unordered_set<StmtNum>{ varID1, varID2 });
+
+    // Check Modifies
+    REQUIRE(PKB::modifies->getStmtsModifies(varID) == unordered_set<ID>{ 1 });
+    REQUIRE(PKB::modifies->getStmtsModifies(varID1) == unordered_set<ID>{ });
 }
 
 TEST_CASE("Nested while-if test - Parser-DE Test") {
@@ -150,7 +202,7 @@ TEST_CASE("Nested while-if test - Parser-DE Test") {
     REQUIRE(PKB::uses->getStmtsUses(condVarId) == unordered_set<StmtNum>{ 2, 4 });
     REQUIRE(PKB::uses->getStmtsUses(varID1) == unordered_set<StmtNum>{ 1, 2, 4, 5 });
     REQUIRE(PKB::uses->getStmtsUses(varID4) == unordered_set<StmtNum>{ 7 });
-    REQUIRE(PKB::uses->getStmtsUses(varID2) == unordered_set<StmtNum>{  });
+    REQUIRE(PKB::uses->getStmtsUses(varID2) == unordered_set<StmtNum>{ });
     REQUIRE(PKB::uses->getVarsUsedByProc(0) == unordered_set<StmtNum>{ condVarId, varID1, varID4 });
 
     // Check Modifies
@@ -158,7 +210,7 @@ TEST_CASE("Nested while-if test - Parser-DE Test") {
     REQUIRE(PKB::modifies->getStmtsModifies(varID2) == unordered_set<ID>{ 2, 4, 5 });
     REQUIRE(PKB::modifies->getStmtsModifies(varID3) == unordered_set<ID>{ 2, 4, 6 });
     REQUIRE(PKB::modifies->getStmtsModifies(varID5) == unordered_set<ID>{ 8 });
-    REQUIRE(PKB::modifies->getStmtsModifies(varID1) == unordered_set<ID>{  });
+    REQUIRE(PKB::modifies->getStmtsModifies(varID1) == unordered_set<ID>{ });
 
 }
 
@@ -237,7 +289,7 @@ TEST_CASE("Triple-nested while test - Parser-DE Test") {
     REQUIRE(PKB::follows->getFollower(2) != 4);
     REQUIRE(PKB::follows->getFollower(2) != 5);
     REQUIRE(PKB::follows->getFollower(2) != 6);
-    REQUIRE(PKB::follows->getFollower(2) != 57);
+    REQUIRE(PKB::follows->getFollower(2) != 7);
     REQUIRE(PKB::follows->getFollower(2) == 8);
     REQUIRE(PKB::follows->getFollower(3) == 4);
     REQUIRE(PKB::follows->getFollower(4) != 5);
@@ -291,20 +343,26 @@ TEST_CASE("Triple-nested if test - Parser-DE Test") {
     /*
     procedure ToTheMoon {
      1   x = 1 + 2 * a;
-     2   while (v > 1) {
+     2   if (v > 1) then {
      3       x = 2;
-     4       while (v == 1) {
+         } else {
+     4       if (v == 1) then {
      5          c = a;
-     6          while (t != 5000) {
+             } else {
+     6          if (t != 5000) then {
      7              b = 2;
+                } else {
+     8              xor = (5 * 2) + 7;
                 }
             }
         }
-     8   print p;
-     9   read s;
+     9   print p;
+    10   read s;
       }
      */
-    string input = "procedure ToTheMoon {x = 1 + 2 * a; while (v > 1) {x = 2; while (v==1) {c = a; while (t != 5000) {b = 2;}}} print p; read s;} ";
+    string input = "procedure ToTheMoon {x = 1 + 2 * a; if (v > 1) then {x = 2;} else { "
+                        "if (v==1) then {c = a;} else {"
+                            "if (t != 5000) then {b = 2;} else {xor = (5 * 2) + 7;}}} print p; read s;} ";
     std::vector<sp::Token> actual_tok;
     std::vector<sp::Token*> tok_ptrs;
     ParserUtils::StringToTokenPtrs(input, actual_tok, tok_ptrs);
@@ -315,14 +373,15 @@ TEST_CASE("Triple-nested if test - Parser-DE Test") {
 
     // Check StmtLst
     REQUIRE(PKB::stmtLstTable->hasStmtLst(1) == true);
-    REQUIRE(PKB::stmtLstTable->hasStmtLst(2) == false); // actual While stmt
-    REQUIRE(PKB::stmtLstTable->hasStmtLst(3) == true); // 1st While's stmtLst
-    REQUIRE(PKB::stmtLstTable->hasStmtLst(4) == false);
-    REQUIRE(PKB::stmtLstTable->hasStmtLst(5) == true); // 2nd While's stmtLst
-    REQUIRE(PKB::stmtLstTable->hasStmtLst(6) == false);
-    REQUIRE(PKB::stmtLstTable->hasStmtLst(7) == true); // 3rd While's stmtLst
-    REQUIRE(PKB::stmtLstTable->hasStmtLst(8) == false);
+    REQUIRE(PKB::stmtLstTable->hasStmtLst(2) == false); // actual if stmt
+    REQUIRE(PKB::stmtLstTable->hasStmtLst(3) == true); // 1st If's stmtLst
+    REQUIRE(PKB::stmtLstTable->hasStmtLst(4) == true); // 1st If's Else stmtLst
+    REQUIRE(PKB::stmtLstTable->hasStmtLst(5) == true); // 2nd If's stmtLst
+    REQUIRE(PKB::stmtLstTable->hasStmtLst(6) == true); // 2nd If's Else stmtLst
+    REQUIRE(PKB::stmtLstTable->hasStmtLst(7) == true); // 3rd If's stmtLst
+    REQUIRE(PKB::stmtLstTable->hasStmtLst(8) == true); // 3rd If's Else stmtLst
     REQUIRE(PKB::stmtLstTable->hasStmtLst(9) == false);
+    REQUIRE(PKB::stmtLstTable->hasStmtLst(10) == false);
 
     // Check varNames
     ID varID = PKB::varTable->getVarID("x");
@@ -337,12 +396,15 @@ TEST_CASE("Triple-nested if test - Parser-DE Test") {
     ID varID3 = PKB::varTable->getVarID("b");
     REQUIRE(varID3 == 5);
     REQUIRE(PKB::varTable->getVarName(varID3) == "b");
-    ID varID4 = PKB::varTable->getVarID("p");
+    ID varID4 = PKB::varTable->getVarID("xor");
     REQUIRE(varID4 == 6);
-    REQUIRE(PKB::varTable->getVarName(varID4) == "p");
-    ID varID5 = PKB::varTable->getVarID("s");
+    REQUIRE(PKB::varTable->getVarName(varID4) == "xor");
+    ID varID5 = PKB::varTable->getVarID("p");
     REQUIRE(varID5 == 7);
-    REQUIRE(PKB::varTable->getVarName(varID5) == "s");
+    REQUIRE(PKB::varTable->getVarName(varID5) == "p");
+    ID varID6 = PKB::varTable->getVarID("s");
+    REQUIRE(varID6 == 8);
+    REQUIRE(PKB::varTable->getVarName(varID6) == "s");
 
     ID condVarId = PKB::varTable->getVarID("v");
     REQUIRE(condVarId == 2);    // varTable ID starts indexing at 0
@@ -355,6 +417,8 @@ TEST_CASE("Triple-nested if test - Parser-DE Test") {
     REQUIRE(PKB::constTable->hasConst("1") == true);
     REQUIRE(PKB::constTable->hasConst("2") == true);
     REQUIRE(PKB::constTable->hasConst("5000") == true);
+    REQUIRE(PKB::constTable->hasConst("5") == true);
+    REQUIRE(PKB::constTable->hasConst("7") == true);
 
     // Check Follows/Follows*
     REQUIRE(PKB::follows->getFollower(1) == 2);
@@ -362,37 +426,48 @@ TEST_CASE("Triple-nested if test - Parser-DE Test") {
     REQUIRE(PKB::follows->getFollower(2) != 4);
     REQUIRE(PKB::follows->getFollower(2) != 5);
     REQUIRE(PKB::follows->getFollower(2) != 6);
-    REQUIRE(PKB::follows->getFollower(2) != 57);
-    REQUIRE(PKB::follows->getFollower(2) == 8);
-    REQUIRE(PKB::follows->getFollower(3) == 4);
+    REQUIRE(PKB::follows->getFollower(2) != 7);
+    REQUIRE(PKB::follows->getFollower(2) != 8);
+    REQUIRE(PKB::follows->getFollower(2) == 9);
+    REQUIRE(PKB::follows->getFollower(2) != 10);
+    REQUIRE(PKB::follows->getFollower(3) != 4);
+    REQUIRE(PKB::follows->getFollower(3) != 5);
+    REQUIRE(PKB::follows->getFollower(3) != 6);
+    REQUIRE(PKB::follows->getFollower(3) != 7);
+    REQUIRE(PKB::follows->getFollower(3) != 8);
     REQUIRE(PKB::follows->getFollower(4) != 5);
     REQUIRE(PKB::follows->getFollower(4) != 6);
     REQUIRE(PKB::follows->getFollower(4) != 7);
-    REQUIRE(PKB::follows->getFollower(5) == 6);
+    REQUIRE(PKB::follows->getFollower(5) != 8);
+    REQUIRE(PKB::follows->getFollower(5) != 6);
+    REQUIRE(PKB::follows->getFollower(5) != 7);
+    REQUIRE(PKB::follows->getFollower(5) != 8);
     REQUIRE(PKB::follows->getFollower(6) != 7);
+    REQUIRE(PKB::follows->getFollower(6) != 8);
     REQUIRE(PKB::follows->getFollower(7) != 8);
-    REQUIRE(PKB::follows->getFollower(8) == 9);
-    REQUIRE(PKB::follows->getFollower(1) != 1);
-    REQUIRE(PKB::follows->getFollowerStar(1) == unordered_set<StmtNum>{ 2, 8, 9 });
-    REQUIRE(PKB::follows->getFollowerStar(2) == unordered_set<StmtNum>{ 8, 9 });
-    REQUIRE(PKB::follows->getFollowerStar(3) == unordered_set<StmtNum>{ 4 });
+    REQUIRE(PKB::follows->getFollower(8) != 9);
+    REQUIRE(PKB::follows->getFollower(1) != 0);
+
+    REQUIRE(PKB::follows->getFollowerStar(1) == unordered_set<StmtNum>{ 2, 9, 10 });
+    REQUIRE(PKB::follows->getFollowerStar(2) == unordered_set<StmtNum>{ 9, 10 });
+    REQUIRE(PKB::follows->getFollowerStar(3) == unordered_set<StmtNum>{ });
     REQUIRE(PKB::follows->getFollowerStar(4) == unordered_set<StmtNum>{ });
-    REQUIRE(PKB::follows->getFollowerStar(5) == unordered_set<StmtNum>{ 6 });
+    REQUIRE(PKB::follows->getFollowerStar(5) == unordered_set<StmtNum>{ });
     REQUIRE(PKB::follows->getFollowerStar(6) == unordered_set<StmtNum>{ });
     REQUIRE(PKB::follows->getFollowerStar(7) == unordered_set<StmtNum>{ });
-    REQUIRE(PKB::follows->getFollowerStar(8) == unordered_set<StmtNum>{ 9 });
-    REQUIRE(PKB::follows->getFollowerStar(9) == unordered_set<StmtNum>{ });
+    REQUIRE(PKB::follows->getFollowerStar(8) == unordered_set<StmtNum>{ });
+    REQUIRE(PKB::follows->getFollowerStar(9) == unordered_set<StmtNum>{ 10 });
 
     // Check Parent/Parent*
     REQUIRE(PKB::parent->getChildren(2) == unordered_set<StmtNum>{ 3, 4 }); // Only testing for Parent, not Parent*
     REQUIRE(PKB::parent->getChildren(4) == unordered_set<StmtNum>{ 5, 6 });
-    REQUIRE(PKB::parent->getChildren(6) == unordered_set<StmtNum>{ 7 });
+    REQUIRE(PKB::parent->getChildren(6) == unordered_set<StmtNum>{ 7, 8 });
     REQUIRE(PKB::parent->getParent(3) == 2);
     REQUIRE(PKB::parent->getParent(5) == 4);
     REQUIRE(PKB::parent->getParent(7) == 6);
-    REQUIRE(PKB::parent->getChildrenStar(2) == unordered_set<StmtNum>{ 3, 4, 5, 6, 7 });
-    REQUIRE(PKB::parent->getChildrenStar(4) == unordered_set<StmtNum>{ 5, 6, 7 });
-    REQUIRE(PKB::parent->getChildrenStar(6) == unordered_set<StmtNum>{ 7 });
+    REQUIRE(PKB::parent->getChildrenStar(2) == unordered_set<StmtNum>{ 3, 4, 5, 6, 7, 8 });
+    REQUIRE(PKB::parent->getChildrenStar(4) == unordered_set<StmtNum>{ 5, 6, 7, 8 });
+    REQUIRE(PKB::parent->getChildrenStar(6) == unordered_set<StmtNum>{ 7, 8 });
 
     // Check Uses
     REQUIRE(PKB::uses->getStmtsUses(condVarId) == unordered_set<StmtNum>{ 2, 4 });
@@ -400,16 +475,17 @@ TEST_CASE("Triple-nested if test - Parser-DE Test") {
     REQUIRE(PKB::uses->getStmtsUses(varID) == unordered_set<StmtNum>{ });
     REQUIRE(PKB::uses->getStmtsUses(varID1) == unordered_set<StmtNum>{ 1, 2, 4, 5 }); // Container stmt
     REQUIRE(PKB::uses->getStmtsUses(varID2) == unordered_set<StmtNum>{ });
-    REQUIRE(PKB::uses->getStmtsUses(varID4) == unordered_set<StmtNum>{ 8 });
-    REQUIRE(PKB::uses->getStmtsUses(varID5) == unordered_set<StmtNum>{ });
-    REQUIRE(PKB::uses->getVarsUsedByProc(0) == unordered_set<StmtNum>{ condVarId, condVarId2, varID1, varID4 });
+    REQUIRE(PKB::uses->getStmtsUses(varID4) == unordered_set<StmtNum>{ });
+    REQUIRE(PKB::uses->getStmtsUses(varID5) == unordered_set<StmtNum>{ 9 });
+    REQUIRE(PKB::uses->getVarsUsedByProc(0) == unordered_set<StmtNum>{ condVarId, condVarId2, varID1, varID5 });
 
     // Check Modifies
     REQUIRE(PKB::modifies->getStmtsModifies(varID) == unordered_set<ID>{ 1, 2, 3 }); // Container stmt
-    REQUIRE(PKB::modifies->getStmtsModifies(varID1) == unordered_set<ID>{  });
+    REQUIRE(PKB::modifies->getStmtsModifies(varID1) == unordered_set<ID>{ });
     REQUIRE(PKB::modifies->getStmtsModifies(varID2) == unordered_set<ID>{ 2, 4, 5 });
     REQUIRE(PKB::modifies->getStmtsModifies(varID3) == unordered_set<ID>{ 2, 4, 6, 7 });
-    REQUIRE(PKB::modifies->getStmtsModifies(varID5) == unordered_set<ID>{ 9 });
+    REQUIRE(PKB::modifies->getStmtsModifies(varID4) == unordered_set<ID>{ 2, 4, 6, 8 });
+    REQUIRE(PKB::modifies->getStmtsModifies(varID6) == unordered_set<ID>{ 10 });
 }
 
 TEST_CASE("Syntactic Errors 1 - Parser-DE Test") {
