c4a131a7cc194c5067d978d9e03b8ab0b1334402
CheyanneSim <CheyanneSim@users.noreply.github.com>
2021-03-05 18:13:56 +0800

    Add Calls

---
+++ Team19/Code19/src/spa/src/PKB/Calls.h
@@ -0,0 +1,113 @@
+#pragma once
+
+#include "../AbstractAPI.h"
+
+class Calls {
+public:
+    // Constructor for CallsTable
+    Calls();
+
+    // Returns true if the program has indirect calls
+    bool hasCyclicalCall();
+
+    // Returns true if Calls(p,q)
+    bool isCalls(ID p, ID q);
+
+    // Returns true if Calls*(p,q)
+    bool isCallsStar(ID p, ID q);
+
+    // Returns the callee of the call statement that occurs at stmtNum. Returns -1 if statement is not a call statement.
+    ID getCalleeInStmt(StmtNum stmtNum);
+
+    // Returns a const reference to the set of p's such that Calls(p,q)
+    unordered_set<ID> const &getCallers(ID q) const;
+
+    // Returns a const reference to the set of q's such that Calls(p,q)
+    unordered_set<ID> const &getCallees(ID p) const;
+
+    // Returns a const reference to the set of p's such that Calls*(p,q)
+    unordered_set<ID> const &getCallersStar(ID q) const;
+
+    // Returns a const reference to the set of q's such that Calls*(p,q)
+    unordered_set<ID> const &getCalleesStar(ID p) const;
+
+    // Returns a const reference to the set of stmts where Calls(p,q) occur
+    unordered_set<StmtNum> const &getStmtsOfCalls(ID p, ID q);
+
+    // Returns a pair of vectors in the callsMap.
+    // First vector is vector of p's. Second is vector of q's.
+    // e.g. if (1,2), (3,4), (5,6) is in callsMap, then it will return <[1,3,5], [2,4,6]>
+    pair<vector<ID>, vector<ID> > getAllCalls();
+
+    // Returns a pair of vectors in the callsStarMap.
+    // First vector is vector of p's. Second is vector of q's.
+    // e.g. if (1,2), (3,4), (5,6) is in callsStarMap, then it will return <[1,3,5], [2,4,6]>
+    pair<vector<ID>, vector<ID> > getAllCallsStar();
+
+    // Returns the no. of pairs of Calls relationship.
+    // E.g. if the callsMap has [1: {2,3}], then the size is 2. Because there are pairs (1,2) and (1,3).
+    int getCallsSize();
+
+    // Returns the no. of pairs of Calls* relationship.
+    // E.g. if the callsStarMap has [1: {2,3}], then the size is 2. Because there are pairs (1,2) and (1,3).
+    int getCallsStarSize();
+
+    // Stores a calls relationship into the callsRawInfoTable. Returns true if the information is successfully added to the PKB.
+    bool storeCalls(StmtNum stmtNum, ID p, ID q);
+
+    // Returns true if the calls statement at stmtNum is already stored in the callsRawInfoTable.
+    bool hasCalls(StmtNum stmtNum);
+
+    // populate the rest of the call tables and handles Modifies and Uses.
+    // Method called after all the calls have been stored in callsRawInfoTable.
+    // Returns true if the processing is successful and there are no cyclical calls detected.
+    bool processCalls();
+
+private:
+
+    // Keeps track of whether the program has cyclical procedure calls
+    bool isCyclic{};
+
+    // The first table to be populated in storeCall(). Info stored will be used to populate the rest of the tables/maps.
+    unordered_map<StmtNum, pair<ID, ID> > callsRawInfoTable;
+
+    // Stores p as key, an unordered map (q as key, stmtNum as value) as value
+    unordered_map<ID, unordered_map<ID, unordered_set<StmtNum> > > callsStmtMap;
+
+    // TODO getter?
+    // Stores StmtNum where Calls(p,q) occurs as key, q as value.
+    unordered_map<StmtNum, ID> callsStmtCalleeMap;
+
+    // Stores p as key, q as value for each Calls(p,q).
+    unordered_map<ID, unordered_set<ID>> callsMap;
+
+    // Stores q as key, p as value for each Calls(p,q).
+    unordered_map<ID, unordered_set<ID>> reverseCallsMap;
+
+    // Stores p as key, q as value for each Calls*(p,q).
+    unordered_map<ID, unordered_set<ID>> callsStarMap;
+
+    // Stores q as key, p as value for each Calls*(p,q).
+    unordered_map<ID, unordered_set<ID>> reverseCallsStarMap;
+
+    // To keep track of the procedures that we have processed in DFS
+    unordered_set<ID> processedProcedures;
+
+    // Stores <p, q> in the callsStarMap.
+    // Stores <q, p> in reverseCallsStarMap
+    // Returns true if info is successfully added.
+    bool storeCallStar(ID p, ID q);
+
+    // calculate all Calls* relationships from the callsMap using BFS and populate the callsStarMap and reverseCallsStarMap
+    void populateCallsStar();
+
+    void updateAllUsesAndModifies();
+
+    void updateUsesAndModifiesForProcedure(ID p);
+
+    // Stores Uses and Modifies relationship for procedure p and statement StmtNum, and container statements within procedure p,
+    // according to the Uses and Modifies relationship of q.
+    void storeUsesAndModifies(StmtNum stmtNum, ID p, ID q);
+
+};
+
---
+++ Team19/Code19/src/unit_testing/src/PKB/TestCalls.cpp
@@ -0,0 +1,243 @@
+#include "PKB/PKB.h"
+#include "PKB/Calls.h"
+
+#include "catch.hpp"
+using namespace std;
+
+//    E.g. p has ID 1, q has ID 2
+//         a has ID 1, b has ID 2, c has ID 3, d has ID 4
+//       procedure p {
+//    1.     call q; --> Uses(p, b), Modifies(p, a), Uses(1, b), Modifies(1, a), Uses(p, d), Modifies(p, c), Uses(1, d), Modifies(1, c)
+//       }
+//
+//       procedure q {
+//    2.     a = b + 1; --> Uses(q, b), Modifies(q, a), Uses(2, b), Modifies(2, a)
+//    3.     call r;    --> Uses(q, d), Modifies(q, c), Uses(3, d), Modifies(3, c)
+//       }
+//
+//       procedure r {
+//    4.     c = d;     --> Uses(r, d), Modifies(r, c), Uses(4, d), Modifies(4, c)
+//       }
+
+void setUpCallsTest() {
+    PKB::resetPKB();
+    PKB::calls->storeCalls(1, 1, 2);
+    PKB::calls->storeCalls(3, 2, 3);
+    PKB::uses->storeStmtUses(2, 2);          // statement 2 uses b
+    PKB::uses->storeProcUses(2, 2);           // procedure q uses b
+    PKB::uses->storeStmtUses(4, 4);         // statement 4 uses d
+    PKB::uses->storeProcUses(3, 4);          // procedure r uses d
+    PKB::modifies->storeStmtModifies(2, 1); // statement 2 modifies a
+    PKB::modifies->storeProcModifies(2, 1);   // procedure q modifies a
+    PKB::modifies->storeStmtModifies(4, 3);  // statement 4 modifies c
+    PKB::modifies->storeProcModifies(3, 3);   // procedure r modifies c
+    PKB::calls->processCalls();
+}
+
+TEST_CASE("storeCalls Test") {
+    Calls* calls = new Calls();
+    REQUIRE(calls->storeCalls(1, 1, 2));
+    REQUIRE(calls->storeCalls(3, 2, 3));
+    REQUIRE_FALSE(calls->storeCalls(1, 4, 5)); // 1 statement can only have 1 call.
+    REQUIRE(calls->storeCalls(2, 1, 2));       // Same procedure can call another procedure multiple times
+}
+
+TEST_CASE("hasCalls Test") {
+    setUpCallsTest();
+    REQUIRE(PKB::calls->hasCalls(1));
+    REQUIRE_FALSE(PKB::calls->hasCalls(2));
+    REQUIRE(PKB::calls->hasCalls(3));
+    REQUIRE_FALSE(PKB::calls->hasCalls(4));
+}
+
+// All tests from now will all test for the logic within processCalls()
+TEST_CASE("hasCyclicalCall Test") {
+    setUpCallsTest();
+    REQUIRE(PKB::calls->processCalls());
+    REQUIRE_FALSE(PKB::calls->hasCyclicalCall());
+
+//    procedure p {
+//        call q;
+//    };
+//
+//    procedure q {
+//        call r;
+//    };
+//
+//    procedure r {
+//        call p;
+//    };
+
+    Calls* calls = new Calls();
+    calls->storeCalls(1, 1, 2);
+    calls->storeCalls(2, 2, 3);
+    calls->storeCalls(3, 3, 1);
+    REQUIRE_FALSE(calls->processCalls());
+    REQUIRE(calls->hasCyclicalCall());
+}
+
+TEST_CASE("isCalls Test") {
+    setUpCallsTest();
+    REQUIRE(PKB::calls->processCalls());
+    REQUIRE(PKB::calls->isCalls(1,2));
+    REQUIRE(PKB::calls->isCalls(2,3));
+    REQUIRE_FALSE(PKB::calls->isCalls(1,3));
+    REQUIRE_FALSE(PKB::calls->isCalls(3,1));
+    REQUIRE_FALSE(PKB::calls->isCalls(2,1));
+    REQUIRE_FALSE(PKB::calls->isCalls(3,2));
+}
+
+TEST_CASE("isCallsStar Test") {
+    setUpCallsTest();
+    REQUIRE(PKB::calls->processCalls());
+    REQUIRE(PKB::calls->isCallsStar(1,2));
+    REQUIRE(PKB::calls->isCallsStar(2,3));
+    REQUIRE(PKB::calls->isCallsStar(1,3));
+    REQUIRE_FALSE(PKB::calls->isCallsStar(3,1));
+    REQUIRE_FALSE(PKB::calls->isCallsStar(2,1));
+    REQUIRE_FALSE(PKB::calls->isCallsStar(3,2));
+}
+
+TEST_CASE("getCalleeInStmt Test") {
+    setUpCallsTest();
+    PKB::calls->storeCalls(5, 4, 1); // suppose procedure s calls procedure p twice (stmt 5 and stmt 6).
+    PKB::calls->storeCalls(6, 4, 1);
+    REQUIRE(PKB::calls->processCalls());
+    REQUIRE(PKB::calls->getCalleeInStmt(1) == 2);
+    REQUIRE(PKB::calls->getCalleeInStmt(3) == 3);
+    REQUIRE(PKB::calls->getCalleeInStmt(5) == 1);
+    REQUIRE(PKB::calls->getCalleeInStmt(6) == 1);
+}
+
+TEST_CASE("getCallers Test") {
+    setUpCallsTest();
+    PKB::calls->storeCalls(5, 4, 2); // suppose we have another procedure s that also calls procedure q.
+    REQUIRE(PKB::calls->processCalls());
+    REQUIRE(PKB::calls->getCallers(1).empty());    // no procedures call p
+    REQUIRE(PKB::calls->getCallers(2) == unordered_set<ID>({1, 4})); // procedure p and s calls q
+    REQUIRE(PKB::calls->getCallers(3) == unordered_set<ID>({2}));    // only procedure q calls r
+}
+
+TEST_CASE("getCallees Test") {
+    setUpCallsTest();
+    PKB::calls->storeCalls(5, 3, 4);
+    PKB::calls->storeCalls(6, 3, 5); // Suppose procedure r calls 2 more procedures
+    REQUIRE(PKB::calls->processCalls());
+    REQUIRE(PKB::calls->getCallees(1) == unordered_set<ID>({2})); // procedure p calls q only
+    REQUIRE(PKB::calls->getCallees(2) == unordered_set<ID>({3})); // procedure q calls r only
+    REQUIRE(PKB::calls->getCallees(3) == unordered_set<ID>({4,5})); // procedure r calls 2 more procedures
+    REQUIRE(PKB::calls->getCallees(4).empty());
+}
+
+TEST_CASE("getCallersStar Test") {
+    setUpCallsTest();
+    PKB::calls->storeCalls(5, 4, 1); // suppose we have another procedure s that also calls procedure p and calls it twice (stmt 5 and 6)
+    PKB::calls->storeCalls(6, 4, 1);
+    REQUIRE(PKB::calls->processCalls());
+    REQUIRE(PKB::calls->getCallersStar(1) == unordered_set<ID>({4})); // procedure s calls p
+    REQUIRE(PKB::calls->getCallersStar(2) == unordered_set<ID>({1, 4})); // procedure p calls q so s calls* q
+    REQUIRE(PKB::calls->getCallersStar(3) == unordered_set<ID>({2, 1, 4}));    // procedure q calls r, so procedure p and s calls* r
+    REQUIRE(PKB::calls->getCallersStar(4).empty()); // no procedure calls/calls* s
+}
+
+TEST_CASE("getCalleesStar Test") {
+    setUpCallsTest();
+    PKB::calls->storeCalls(5, 4, 1); // suppose we have another procedure s that also calls procedure p.
+    REQUIRE(PKB::calls->processCalls());
+    REQUIRE(PKB::calls->getCalleesStar(1) == unordered_set<ID>({2, 3})); // procedure p calls q and calls* r
+    REQUIRE(PKB::calls->getCalleesStar(2) == unordered_set<ID>({3})); // procedure q calls r only
+    REQUIRE(PKB::calls->getCalleesStar(3).empty()); // procedure r does not call any procedures
+    REQUIRE(PKB::calls->getCalleesStar(4) == unordered_set<ID>({1, 2, 3})); // s calls p, thus it calls* q and r
+}
+
+TEST_CASE("getStmtsOfCalls test") {
+    setUpCallsTest();
+    PKB::calls->storeCalls(5, 4, 1); // suppose procedure s calls procedure p twice (stmt 5 and stmt 6).
+    PKB::calls->storeCalls(6, 4, 1);
+    REQUIRE(PKB::calls->processCalls());
+    REQUIRE(PKB::calls->getStmtsOfCalls(1, 2) == unordered_set<StmtNum>({1})); // p only call q in stmt 1
+    REQUIRE(PKB::calls->getStmtsOfCalls(2, 3) == unordered_set<StmtNum>({3})); // q only call r in stmt 3
+    REQUIRE(PKB::calls->getStmtsOfCalls(4, 1) == unordered_set<StmtNum>({5, 6})); // s calls p in stmt 5 and 6
+}
+
+TEST_CASE("getCallsSize Test") {
+    setUpCallsTest();
+    REQUIRE(PKB::calls->processCalls());
+    REQUIRE(PKB::calls->getCallsSize() == 2);
+    setUpCallsTest();
+    PKB::calls->storeCalls(5, 4, 1); // suppose procedure s calls procedure p twice (stmt 5 and stmt 6).
+    PKB::calls->storeCalls(6, 4, 1);
+    REQUIRE(PKB::calls->processCalls());
+    REQUIRE(PKB::calls->getCallsSize() == 3);
+}
+
+TEST_CASE("getCallsStarSize Test") {
+    setUpCallsTest();
+    REQUIRE(PKB::calls->processCalls());
+    REQUIRE(PKB::calls->getCallsStarSize() == 3);
+    setUpCallsTest();
+    PKB::calls->storeCalls(5, 4, 1); // suppose procedure s calls procedure p twice (stmt 5 and stmt 6).
+    PKB::calls->storeCalls(6, 4, 1);
+    REQUIRE(PKB::calls->processCalls());
+    REQUIRE(PKB::calls->getCallsStarSize() == 6);
+}
+
+TEST_CASE("getAllCalls Test") {
+    setUpCallsTest();
+    PKB::calls->storeCalls(5, 4, 1); // suppose procedure s calls procedure p twice (stmt 5 and stmt 6).
+    PKB::calls->storeCalls(6, 4, 1);
+    REQUIRE(PKB::calls->processCalls());
+    pair<vector<ID>, vector<ID> > result = PKB::calls->getAllCalls();
+    vector<ID> ps = result.first;
+    vector<ID> qs = result.second;
+    int num_pairs = ps.size();
+    // Check that it has correct number of pairs
+    REQUIRE(num_pairs == PKB::calls->getCallsSize());
+    REQUIRE(qs.size() == num_pairs);
+    // check that each pair at the same index has Calls relationship
+    for (int i = 0; i < num_pairs; i++) {
+        REQUIRE(PKB::calls->isCalls(ps.at(i), qs.at(i)));
+    }
+}
+
+TEST_CASE("getAllCallsStar Test") {
+    setUpCallsTest();
+    PKB::calls->storeCalls(5, 4, 1); // suppose procedure s calls procedure p twice (stmt 5 and stmt 6).
+    PKB::calls->storeCalls(6, 4, 1);
+    REQUIRE(PKB::calls->processCalls());
+    pair<vector<ID>, vector<ID> > result = PKB::calls->getAllCallsStar();
+    vector<ID> ps = result.first;
+    vector<ID> qs = result.second;
+    int num_pairs = ps.size();
+    // Check that it has correct number of pairs
+    REQUIRE(num_pairs == PKB::calls->getCallsStarSize());
+    REQUIRE(qs.size() == num_pairs);
+    // check that each pair at the same index has Calls relationship
+    for (int i = 0; i < num_pairs; i++) {
+        REQUIRE(PKB::calls->isCallsStar(ps.at(i), qs.at(i)));
+    }
+}
+
+TEST_CASE("check uses and modifies") {
+    setUpCallsTest();
+    REQUIRE(PKB::calls->processCalls());
+    REQUIRE(PKB::uses->getProcsUses(1).empty()); // no procedure uses a
+    REQUIRE(PKB::uses->getProcsUses(2) == unordered_set<ID>({1, 2})); // procedure p and q use b
+    REQUIRE(PKB::uses->getProcsUses(3).empty()); // no procedure uses c
+    REQUIRE(PKB::uses->getProcsUses(4) == unordered_set<ID>({1, 2, 3})); // all procedure use d
+
+    REQUIRE(PKB::uses->getStmtsUses(1).empty()); // no statements uses a
+    REQUIRE(PKB::uses->getStmtsUses(2) == unordered_set<ID>({1, 2})); // statements 1 and 2 use b
+    REQUIRE(PKB::uses->getStmtsUses(3).empty()); // no statements uses c
+    REQUIRE(PKB::uses->getStmtsUses(4) == unordered_set<ID>({1, 3, 4})); // statements 1,3,4 use d
+
+    REQUIRE(PKB::modifies->getProcsModifies(1) == unordered_set<ID>({1, 2})); // procedure p and q modify a
+    REQUIRE(PKB::modifies->getProcsModifies(2).empty()); // no procedure modifies b
+    REQUIRE(PKB::modifies->getProcsModifies(3) == unordered_set<ID>({1, 2, 3})); // all procedures modify c
+    REQUIRE(PKB::modifies->getProcsModifies(4).empty()); // no procedure modifies d
+
+    REQUIRE(PKB::modifies->getStmtsModifies(1) == unordered_set<ID>({1, 2})); // statement 1 and 2 modify a
+    REQUIRE(PKB::modifies->getStmtsModifies(2).empty()); // no statement modifies b
+    REQUIRE(PKB::modifies->getStmtsModifies(3) == unordered_set<ID>({1, 3, 4})); // statement 1,3,4 modify c
+    REQUIRE(PKB::modifies->getStmtsModifies(4).empty()); // no statement modifies d
+}
