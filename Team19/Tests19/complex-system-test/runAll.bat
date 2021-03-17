fd397da9fd14834c0f539ffb991b0ad859cee937
Ang Lu Ling <ang.lu.shing99@gmail.com>
2021-03-17 13:16:06 +0800

    add calls* test for complex-system-test

--- Team19/Tests19/complex-system-test/runAll.bat
+++ Team19/Tests19/complex-system-test/runAll.bat
@@ -5,12 +5,16 @@ echo Start system tests...
 if exist ..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe (
 	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe Source.txt Boolean_query.txt Boolean_out.xml
 	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe Source.txt Calls_query.txt Calls_out.xml
+	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe Source.txt CallsStar_query.txt CallsStar_out.xml
 	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe Source.txt Next_query.txt Next_out.xml
+	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe Source.txt NextStar_query.txt NextStar_out.xml
 ) else (
 	set /p id=Enter path to your autotester:
 	%id% Source.txt Boolean_query.txt Boolean_out.xml
 	%id% Source.txt Calls_query.txt Calls_out.xml
-	%id% Source.txt Next_query.txt Next_out.xml
+	%id% Source.txt CallsStar_query.txt CallsStar_out.xml
+	%id% Source.txt Next_query.txt Next_out.xml	
+	%id% Source.txt NextStar_query.txt NextStar_out.xml
 
 ) 
 
--- Team19/Tests19/complex-system-test/runAll.sh
+++ Team19/Tests19/complex-system-test/runAll.sh
@@ -6,12 +6,16 @@ FILE=../../Code19/cmake-build-debug/src/autotester/autotester
 if [ -f "$FILE" ]; then
 	../../Code19/cmake-build-debug/src/autotester/autotester Source.txt Boolean_query.txt Boolean_out.xml
 	../../Code19/cmake-build-debug/src/autotester/autotester Source.txt Calls_query.txt Calls_out.xml
+	../../Code19/cmake-build-debug/src/autotester/autotester Source.txt CallsStar_query.txt CallsStar_out.xml
 	../../Code19/cmake-build-debug/src/autotester/autotester Source.txt Next_query.txt Next_out.xml
+	../../Code19/cmake-build-debug/src/autotester/autotester Source.txt NextStar_query.txt NextStar_out.xml
 else 
 	echo "Enter path to autotester: "
 	read id
 	$id Source.txt Boolean_query.txt Boolean_out.xml
 	$id Source.txt Calls_query.txt Calls_out.xml
+	$id Source.txt CallsStar_query.txt CallsStar_out.xml
 	$id Source.txt Next_query.txt Next_out.xml
+	$id Source.txt NextStar_query.txt NextStar_out.xml
 fi
 read -p "Press any key to resume ..."
