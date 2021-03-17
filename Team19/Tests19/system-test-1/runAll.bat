f4f4f5a1b7631d4f0fcb0c913573ca371046ced0
Ang Lu Ling <ang.lu.shing99@gmail.com>
2021-03-17 10:48:01 +0800

    resolve conflict

--- Team19/Tests19/system-test-1/runAll.bat
+++ Team19/Tests19/system-test-1/runAll.bat
@@ -1,10 +1,18 @@
 @echo off
 echo Start system tests...
 
-set /p id=Enter path to autotester: 
-%id% Source.txt Boolean_query.txt Boolean_out.xml
-%id% Source.txt Calls_query.txt Calls_out.xml
-%id% Source.txt Next_query.txt Next_out.xml
+
+if exist ..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe (
+	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe Source.txt Boolean_query.txt Boolean_out.xml
+	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe Source.txt Calls_query.txt Calls_out.xml
+	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe Source.txt Next_query.txt Next_out.xml
+) else (
+	set /p id=Enter path to your autotester:
+	%id% Source.txt Boolean_query.txt Boolean_out.xml
+	%id% Source.txt Calls_query.txt Calls_out.xml
+	%id% Source.txt Next_query.txt Next_out.xml
+
+) 
 
 pause
 
--- Team19/Tests19/system-test-1/runAll.sh
+++ Team19/Tests19/system-test-1/runAll.sh
@@ -2,10 +2,16 @@
 
 echo Start system tests...
 
-echo "Enter path to autotester: "
-read id
-$id Source.txt Boolean_query.txt Boolean_out.xml
-$id Source.txt Calls_query.txt Calls_out.xml
-$id Source.txt Next_query.txt Next_out.xml
-
+FILE=../../Code19/cmake-build-debug/src/autotester/autotester
+if [ -f "$FILE" ]; then
+	../../Code19/cmake-build-debug/src/autotester/autotester Source.txt Boolean_query.txt Boolean_out.xml
+	../../Code19/cmake-build-debug/src/autotester/autotester Source.txt Calls_query.txt Calls_out.xml
+	../../Code19/cmake-build-debug/src/autotester/autotester Source.txt Next_query.txt Next_out.xml
+else 
+	echo "Enter path to autotester: "
+	read id
+	$id Source.txt Boolean_query.txt Boolean_out.xml
+	$id Source.txt Calls_query.txt Calls_out.xml
+	$id Source.txt Next_query.txt Next_out.xml
+fi
 read -p "Press any key to resume ..."
