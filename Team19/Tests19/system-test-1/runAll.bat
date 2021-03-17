3290de27c97efd6d034055488824fa9dbe7ef1b1
CheyanneSim <CheyanneSim@users.noreply.github.com>
2021-03-17 10:52:06 +0800

    Add Next* test for system-test-1

--- Team19/Tests19/system-test-1/runAll.bat
+++ Team19/Tests19/system-test-1/runAll.bat
@@ -5,6 +5,7 @@ set /p id=Enter path to autotester:
 %id% Source.txt Boolean_query.txt Boolean_out.xml
 %id% Source.txt Calls_query.txt Calls_out.xml
 %id% Source.txt Next_query.txt Next_out.xml
+%id% Source.txt NextStar_query.txt NextStar_out.xml
 
 pause
 
--- Team19/Tests19/system-test-1/runAll.sh
+++ Team19/Tests19/system-test-1/runAll.sh
@@ -7,5 +7,6 @@ read id
 $id Source.txt Boolean_query.txt Boolean_out.xml
 $id Source.txt Calls_query.txt Calls_out.xml
 $id Source.txt Next_query.txt Next_out.xml
+$id Source.txt NextStar_query.txt NextStar_out.xml
 
 read -p "Press any key to resume ..."
