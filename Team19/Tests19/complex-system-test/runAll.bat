0a158b1622e48290856d7b0f5ffa9098aec5c0d6
Ang Lu Ling <ang.lu.shing99@gmail.com>
2021-03-18 17:10:16 +0800

    add modifies, uses, pattern test for complex

--- Team19/Tests19/complex-system-test/runAll.bat
+++ Team19/Tests19/complex-system-test/runAll.bat
@@ -8,6 +8,9 @@ if exist ..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe (
 	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe Source.txt CallsStar_query.txt CallsStar_out.xml
 	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe Source.txt Next_query.txt Next_out.xml
 	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe Source.txt NextStar_query.txt NextStar_out.xml
+	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe Source.txt Modifies_query.txt Modifies_out.xml
+	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe Source.txt Uses_query.txt Uses_out.xml
+	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe Source.txt Pattern_query.txt Pattern_out.xml
 ) else (
 	set /p id=Enter path to your autotester:
 	%id% Source.txt Boolean_query.txt Boolean_out.xml
@@ -15,6 +18,9 @@ if exist ..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe (
 	%id% Source.txt CallsStar_query.txt CallsStar_out.xml
 	%id% Source.txt Next_query.txt Next_out.xml	
 	%id% Source.txt NextStar_query.txt NextStar_out.xml
+	%id% Source.txt Modifies_query.txt Modifies_out.xml
+	%id% Source.txt Uses_query.txt Uses_out.xml
+	%id% Source.txt Pattern_query.txt Pattern_out.xml
 
 ) 
 
--- Team19/Tests19/complex-system-test/runAll.sh
+++ Team19/Tests19/complex-system-test/runAll.sh
@@ -9,6 +9,9 @@ if [ -f "$FILE" ]; then
 	../../Code19/cmake-build-debug/src/autotester/autotester Source.txt CallsStar_query.txt CallsStar_out.xml
 	../../Code19/cmake-build-debug/src/autotester/autotester Source.txt Next_query.txt Next_out.xml
 	../../Code19/cmake-build-debug/src/autotester/autotester Source.txt NextStar_query.txt NextStar_out.xml
+	../../Code19/cmake-build-debug/src/autotester/autotester Source.txt Modifies_query.txt Modifies_out.xml
+	../../Code19/cmake-build-debug/src/autotester/autotester Source.txt Uses_query.txt Uses_out.xml
+	../../Code19/cmake-build-debug/src/autotester/autotester Source.txt Pattern_query.txt Pattern_out.xml
 else 
 	echo "Enter path to autotester: "
 	read id
@@ -17,5 +20,8 @@ else
 	$id Source.txt CallsStar_query.txt CallsStar_out.xml
 	$id Source.txt Next_query.txt Next_out.xml
 	$id Source.txt NextStar_query.txt NextStar_out.xml
+	$id Source.txt Modifies_query.txt Modifies_out.xml
+	$id Source.txt Uses_query.txt Uses_out.xml
+	$id Source.txt Pattern_query.txt Pattern_out.xml
 fi
 read -p "Press any key to resume ..."
