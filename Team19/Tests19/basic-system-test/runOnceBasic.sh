1d1f0976a6581aa8e541b962c68ce5b08b3ad574
uwu <e0031374@gmail.com>
2021-03-22 16:24:49 +0800

    Remove Source.txt

--- Team19/Tests19/basic-system-test/runOnceBasic.sh
+++ Team19/Tests19/basic-system-test/runOnceBasic.sh
@@ -4,7 +4,7 @@ FILE=../../Code19/build_win/x86-Debug/src/autotester/autotester.exe
 if [ -f "$FILE" ]; then
 	echo "Enter query eg: Next for Next_query.txt"
 	read query
-	../../Code19/build_win/x86-Debug/src/autotester/autotester.exe ./Source.txt ./${query}_query.txt ./${query}_out.xml
+	../../Code19/build_win/x86-Debug/src/autotester/autotester.exe ./basicFullSimple_source.txt ./${query}_query.txt ./${query}_out.xml
 
 else
 	echo "path to autotester not found"
