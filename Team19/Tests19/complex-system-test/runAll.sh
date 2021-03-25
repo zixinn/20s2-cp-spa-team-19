2deef71a2d81d5b14d593413d33f127470313345
CheyanneSim <CheyanneSim@users.noreply.github.com>
2021-03-25 13:13:15 +0800

    Add test for Follows for complex-system-test

--- Team19/Tests19/complex-system-test/runAll.sh
+++ Team19/Tests19/complex-system-test/runAll.sh
@@ -18,6 +18,7 @@ if [ -f "$FILE" ]; then
 	../../Code19/cmake-build-debug/src/autotester/autotester complexFullSimple_source.txt Affects_query.txt Affects_out.xml
 	../../Code19/cmake-build-debug/src/autotester/autotester complexFullSimple_source.txt AffectsStar_query.txt AffectsStar_out.xml
 	../../Code19/cmake-build-debug/src/autotester/autotester complexFullSimple_source.txt SelectAttribute_query.txt SelectAttribute_out.xml
+	../../Code19/cmake-build-debug/src/autotester/autotester complexFullSimple_source.txt Follows_query.txt Follows_out.xml
 else 
 	echo "Enter path to autotester: "
 	read id
@@ -35,5 +36,6 @@ else
 	$id complexFullSimple_source.txt Affects_query.txt Affects_out.xml
 	$id complexFullSimple_source.txt AffectsStar_query.txt AffectsStar_out.xml
 	$id complexFullSimple_source.txt SelectAttribute_query.txt SelectAttribute_out.xml
+	$id complexFullSimple_source.txt Follows_query.txt Follows_out.xml
 fi
 read -p "Press any key to resume ..."
