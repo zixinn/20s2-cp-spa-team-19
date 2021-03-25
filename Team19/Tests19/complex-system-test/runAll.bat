98b3cacde4d3fabae43e0cddc349177970685ab1
CheyanneSim <CheyanneSim@users.noreply.github.com>
2021-03-25 14:37:21 +0800

    Add test for Follows* for complex-system-test

--- Team19/Tests19/complex-system-test/runAll.bat
+++ Team19/Tests19/complex-system-test/runAll.bat
@@ -18,6 +18,7 @@ if exist ..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe (
 	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe complexFullSimple_source.txt AffectsStar_query.txt AffectsStar_out.xml
 	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe complexFullSimple_source.txt SelectAttribute_query.txt SelectAttribute_out.xml
 	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe complexFullSimple_source.txt Follows_query.txt Follows_out.xml
+	..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe complexFullSimple_source.txt FollowsStar_query.txt FollowsStar_out.xml
 ) else (
 	set /p id=Enter path to your autotester:
 	%id% complexFullSimple_source.txt Boolean_query.txt Boolean_out.xml
@@ -35,6 +36,7 @@ if exist ..\..\Code19\build_win\x86-Debug\src\autotester\autotester.exe (
 	%id% complexFullSimple_source.txt AffectsStar_query.txt AffectsStar_out.xml
 	%id% complexFullSimple_source.txt SelectAttribute_query.txt SelectAttribute_out.xml
 	%id% complexFullSimple_source.txt Follows_query.txt Follows_out.xml
+	%id% complexFullSimple_source.txt FollowsStar_query.txt FollowsStar_out.xml
 ) 
 
 pause
--- Team19/Tests19/complex-system-test/runAll.sh
+++ Team19/Tests19/complex-system-test/runAll.sh
@@ -19,6 +19,7 @@ if [ -f "$FILE" ]; then
 	../../Code19/cmake-build-debug/src/autotester/autotester complexFullSimple_source.txt AffectsStar_query.txt AffectsStar_out.xml
 	../../Code19/cmake-build-debug/src/autotester/autotester complexFullSimple_source.txt SelectAttribute_query.txt SelectAttribute_out.xml
 	../../Code19/cmake-build-debug/src/autotester/autotester complexFullSimple_source.txt Follows_query.txt Follows_out.xml
+	../../Code19/cmake-build-debug/src/autotester/autotester complexFullSimple_source.txt FollowsStar_query.txt FollowsStar_out.xml
 else 
 	echo "Enter path to autotester: "
 	read id
@@ -37,5 +38,6 @@ else
 	$id complexFullSimple_source.txt AffectsStar_query.txt AffectsStar_out.xml
 	$id complexFullSimple_source.txt SelectAttribute_query.txt SelectAttribute_out.xml
 	$id complexFullSimple_source.txt Follows_query.txt Follows_out.xml
+	$id complexFullSimple_source.txt FollowsStar_query.txt FollowsStar_out.xml
 fi
 read -p "Press any key to resume ..."
