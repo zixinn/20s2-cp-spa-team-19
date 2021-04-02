22b052cb13928126d16bc1da6a53065937eeab63
ambervoong <voongyuxuan@gmail.com>
2021-04-02 17:59:14 +0800

    Fix typedefs/indentation/comments for all AST files

--- Team19/Code19/src/spa/src/AST/RelExpr.h
+++ Team19/Code19/src/spa/src/AST/RelExpr.h
@@ -1,29 +1,29 @@
 #pragma once
-#include <string>
 #include "SP/Token.h"
 #include "AST/CondExpr.h"
 
+// AST representing a Relational Conditional Expression
 namespace ast {
-	class RelExpr : public CondExpr {
-		Expr* left;
-		Expr* right;
-	public:
-		RelExpr(sp::Token* token, Expr* left, Expr* right)
-			:CondExpr( token ), left{ left }, right{ right } {}
-		Expr* getLeft() { return left; }
-		Expr* getRight() { return right; }
+    class RelExpr : public CondExpr {
+        Expr* left;
+        Expr* right;
+    public:
+        RelExpr(sp::Token* token, Expr* left, Expr* right)
+                :CondExpr( token ), left{ left }, right{ right } {}
+        Expr* getLeft() { return left; }
+        Expr* getRight() { return right; }
 
-		std::string toString() override {
-			std::string left_str = left ? left->toString() : "[NULL_PTR]";
-			std::string right_str = right ? right->toString() : "[NULL_PTR]";
-			//return "(" + left_str + " " + this->getTokenLiteral() + " " + right_str + ")";
-			return left_str + " " + this->getTokenLiteral() + " " + right_str;
-		}
+        STRING toString() override {
+            STRING left_str = left ? left->toString() : "[NULL_PTR]";
+            STRING right_str = right ? right->toString() : "[NULL_PTR]";
+            //return "(" + left_str + " " + this->getTokenLiteral() + " " + right_str + ")";
+            return left_str + " " + this->getTokenLiteral() + " " + right_str;
+        }
 
-		~RelExpr() {
-			delete left;
-			delete right;
-		}
+        ~RelExpr() {
+            delete left;
+            delete right;
+        }
 
-	};
+    };
 }
--- Team19/Code19/src/spa/src/SP/Token.h
+++ Team19/Code19/src/spa/src/SP/Token.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "../AbstractAPI.h"
 #include <string>
 
 using namespace std;
