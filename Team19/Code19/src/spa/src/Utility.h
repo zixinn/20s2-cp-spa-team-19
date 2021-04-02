68d3503dc39b6f63ae23e370620f354cf99665ba
ambervoong <voongyuxuan@gmail.com>
2021-04-02 20:42:25 +0800

    Revert "update utility typedefs"
    This reverts commit fdbbaccb5b9f5dc71a773c1c965d2b62994a398c.

--- Team19/Code19/src/spa/src/Utility.h
+++ Team19/Code19/src/spa/src/Utility.h
@@ -3,21 +3,19 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include "AbstractAPI.h"
-
-// A collection of utility functions used in SPA
+#include <string>
 
 // Trims leading and trailing spaces from string s
-STRING trim(STRING s);
+std::string trim(std::string s);
 
 // Splits string s by the given delimiter and returns vector containing strings split
 // Strings in vector will be trimmed
-std::vector<STRING> split(STRING s, STRING delimiter);
+std::vector<std::string> split(std::string s, std::string delimiter);
 
 // Checks LETTER(LETTER | DIGIT)*
 // Returns true if name is valid, false otherwise
-bool checkName(STRING s);
+bool checkName(std::string s);
 
 // Checks DIGIT+
 // Returns true if integer is valid, false otherwise
-bool checkInteger(STRING s);
+bool checkInteger(std::string s);
